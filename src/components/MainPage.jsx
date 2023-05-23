06133e4acc54a690c6b64418238e374133d9f16a
Alexey <a99lex@yandex.ru>
2023-05-23 16:19:21 +0300

    add react hook form validation and create Readme file

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,74 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const MainPage = () => {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [telephone, setTelephone] = useState("");
-  const [userNameDirty, setUserNameDirty] = useState(false);
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [telephoneDirty, setTelephoneDirty] = useState(false);
-  const [userNameError, setUserNameError] = useState(
-    "Имя не должно быть пустым"
-  );
-  const [emailError, setEmailError] = useState("Email не должен быть пустым");
-  const [telephoneError, setTelephoneError] = useState(
-    "Телефон не должен быть пустым"
-  );
-  const [formValid, setFormValid] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
 
-  useEffect(() => {
-    if (emailError || telephoneError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [emailError, telephoneError]);
-
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!re.test(String(e.target.value).toLocaleLowerCase())) {
-      setEmailError("Не корректный email");
-    } else {
-      setEmailError("");
-    }
-  };
-  const handleUserName = (e) => {
-    setUserName(e.target.value);
-    if (!e.target.value) {
-      setUserNameError("Имя не должно быть пустым");
-    } else {
-      setUserNameError("");
-    }
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+    reset();
   };
 
-  const handleTelephone = (e) => {
-    setTelephone(e.target.value);
-    if (e.target.value.length < 11) {
-      setTelephoneError("Номер должен быть более 11 символов");
-      if (!e.target.value) {
-        setTelephoneError("Телефон не должен быть пустым");
-      }
-    } else {
-      setTelephoneError("");
-    }
-  };
-
-  const handleBlur = (e) => {
-    switch (e.target.name) {
-      case "name":
-        setUserNameDirty(true);
-        break;
-      case "email":
-        setEmailDirty(true);
-        break;
-      case "telephone":
-        setTelephoneDirty(true);
-        break;
-      default:
-    }
-  };
   return (
     <div className="main__page">
       <div className="main__page-container _container">
@@ -83,62 +30,88 @@ const MainPage = () => {
         </div>
         <div className="main__page-form form">
           <h2 className="form__title">Заказать демонтаж</h2>
-          <form action="#" className="form__feedback">
+          <form onSubmit={handleSubmit(onSubmit)} className="form__feedback">
             <div className="form__box">
-              {userNameDirty && userNameError && (
-                <div className="error__message">{userNameError}</div>
+              {errors?.userName && (
+                <div className="error__message">
+                  {errors.userName.message || "Введите значение"}
+                </div>
               )}
               <input
                 type="text"
                 placeholder="Ваше имя"
                 className="form__input"
-                name="name"
-                value={userName}
-                onBlur={(e) => handleBlur(e)}
-                onChange={(e) => handleUserName(e)}
+                {...register("userName", {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                    value: 3,
+                    message: "Имя должно содержать больше 3 символов",
+                  },
+                })}
               />
             </div>
             <div className="form__box">
-              {telephoneDirty && telephoneError && (
-                <div className="error__message">{telephoneError}</div>
+              {errors?.telephoneValue && (
+                <div className="error__message">
+                  {errors.telephoneValue.message}
+                </div>
               )}
               <input
                 type="text"
                 placeholder="Телефон"
                 className="form__input"
-                name="telephone"
-                value={telephone}
-                onChange={(e) => handleTelephone(e)}
-                onBlur={(e) => handleBlur(e)}
+                {...register("telephoneValue", {
+                  required: "Поле обязательно для заполнения",
+                  pattern: {
+                    value: /^\+7[0-9]{10}$/,
+                    message: "Формат ввода +7**********",
+                  },
+                })}
               />
             </div>
             <div className="form__box">
-              {emailDirty && emailError && (
-                <div className="error__message">{emailError}</div>
+              {errors?.emailValue && (
+                <div className="error__message">
+                  {errors.emailValue.message}
+                </div>
               )}
               <input
                 type="email"
                 placeholder="E-mail"
                 className="form__input"
-                name="email"
-                onChange={(e) => handleEmail(e)}
-                value={email}
-                onBlur={(e) => handleBlur(e)}
+                {...register("emailValue", {
+                  required: "Поле обязательно для заполнения",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
               />
             </div>
+
             <div className="form__agreement agreement">
-              <input type="checkbox" className="agreement__input" />
+              <input
+                type="checkbox"
+                {...register("checkboxValue", {
+                  required: "Argument is required",
+                })}
+                className="agreement__input"
+              />
               <div className="agreement__text">
                 Ознакомлен(а) <span>с пользовательским соглашением</span>
               </div>
+              {errors?.checkboxValue && (
+                <div className="error__message-checkbox">
+                  {errors.checkboxValue.message}
+                </div>
+              )}
             </div>
-            <button
-              disabled={!formValid}
+
+            <input
               type="submit"
+              value="Отправить заявку"
               className="form__button"
-            >
-              Отправить заявку
-            </button>
+            />
           </form>
         </div>
       </div>
